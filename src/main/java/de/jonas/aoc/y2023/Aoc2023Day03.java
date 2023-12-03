417fabf2a81d559e8c12e16115c96fb421d54f15
j00nas <jonas@lohmueller1.de>
2023-12-03 20:31:58 +0100

    added Day03 Solution

--- src/main/java/de/jonas/aoc/y2023/Aoc2023Day03.java
+++ src/main/java/de/jonas/aoc/y2023/Aoc2023Day03.java
@@ -96,8 +96,6 @@ public class Aoc2023Day03 extends Aoc2023Day00 implements DailySolution {
 			engineMap.add(tempArrayList);
 		}
 		
-		engineMap.forEach((x) -> System.out.println(x));
-		
 		ArrayList<Character> numbers = new ArrayList<Character>();		
 		for(int k = 0; k <= 9; k++) {
 			numbers.add(Integer.toString(k).charAt(0));
@@ -105,10 +103,9 @@ public class Aoc2023Day03 extends Aoc2023Day00 implements DailySolution {
 
 		String possNumber = "";
 		boolean numberCounts = false;
-		int ans = 0;
+		long ans = 0;
 		
 		HashMap<String, Integer[]> starHashmap = new HashMap<String, Integer[]>();
-		//ArrayList<Integer> TempStarCoordinates = new ArrayList<Integer>();
 		HashMap<String, Integer> TempStarCoordinates = new HashMap<String, Integer>();
 				
 		for(int i = 0; i < engineMap.size(); i++) {
@@ -117,66 +114,68 @@ public class Aoc2023Day03 extends Aoc2023Day00 implements DailySolution {
 					possNumber += engineMap.get(i).get(j);						
 					//check left
 					if(j > 0) {
-						if(engineMap.get(i).get(j-1) == '*' && !numbers.contains(engineMap.get(i).get(j-1))) {
+						if(engineMap.get(i).get(j-1) == '*') {
 							numberCounts = true;
 							putTempStarCoordinates(TempStarCoordinates, i, j-1);
 						}
 					}
 					//check right
 					if(j < engineMap.get(0).size()-1) {
-						if(engineMap.get(i).get(j+1) == '*' && !numbers.contains(engineMap.get(i).get(j+1))) {
+						if(engineMap.get(i).get(j+1) == '*') {
 							numberCounts = true;
 							putTempStarCoordinates(TempStarCoordinates, i, j+1);
 						}
 					}
 					//check top
 					if(i > 0) {
-						if(engineMap.get(i-1).get(j) == '*' && !numbers.contains(engineMap.get(i-1).get(j))) {
+						if(engineMap.get(i-1).get(j) == '*') {
 							numberCounts = true;
 							putTempStarCoordinates(TempStarCoordinates, i-1, j);
 						}
 					}
 					//check bottom
 					if(i < engineMap.size()-1) {
-						if(engineMap.get(i+1).get(j) == '*' && !numbers.contains(engineMap.get(i+1).get(j))) {
+						if(engineMap.get(i+1).get(j) == '*') {
 							numberCounts = true;
 							putTempStarCoordinates(TempStarCoordinates, i+1, j);
 						}
 					}
 					//check upper left
 					if(i > 0 && j > 0) {
-						if(engineMap.get(i-1).get(j-1) == '*' && !numbers.contains(engineMap.get(i-1).get(j-1))) {
+						if(engineMap.get(i-1).get(j-1) == '*') {
 							numberCounts = true;
 							putTempStarCoordinates(TempStarCoordinates, i-1, j-1);
 						}
 					}
 					//check bottom left
 					if(i < engineMap.size()-1 && j > 0) {
-						if(engineMap.get(i+1).get(j-1) == '*' && !numbers.contains(engineMap.get(i+1).get(j-1))) {
+						if(engineMap.get(i+1).get(j-1) == '*') {
 							numberCounts = true;
 							putTempStarCoordinates(TempStarCoordinates, i+1, j-1);
 						}
 					}
 					//check upper right
 					if(i > 0 && j < engineMap.get(0).size()-1) {
-						if(engineMap.get(i-1).get(j+1) == '*' && !numbers.contains(engineMap.get(i-1).get(j+1))) {
+						if(engineMap.get(i-1).get(j+1) == '*') {
 							numberCounts = true;
 							putTempStarCoordinates(TempStarCoordinates, i-1, j+1);
 						}
 					}
 					//check bottom right
 					if(i < engineMap.size()-1 && j < engineMap.get(0).size()-1) {
-						if(engineMap.get(i+1).get(j+1) == '*' && !numbers.contains(engineMap.get(i+1).get(j+1))) {
+						if(engineMap.get(i+1).get(j+1) == '*') {
 							numberCounts = true;
 							putTempStarCoordinates(TempStarCoordinates, i+1, j+1);
 						}
 					}					
 				}else {		
-					if(numberCounts) {							
+					if(numberCounts) {	
 						for(String coordinates : TempStarCoordinates.keySet()) {							
-							//System.out.println(coordinates + " " + possNumber);
 							if(starHashmap.containsKey(coordinates)) {
 								Integer[] newEntry = new Integer[starHashmap.get(coordinates).length + 1];
+								for(int k = 0; k < starHashmap.get(coordinates).length; k++) {
+									newEntry[k] = starHashmap.get(coordinates)[k];
+								}
 								newEntry[starHashmap.get(coordinates).length] = Integer.parseInt(possNumber);
 								starHashmap.put(coordinates, newEntry);
 							}else {
@@ -192,20 +191,19 @@ public class Aoc2023Day03 extends Aoc2023Day00 implements DailySolution {
 				}
 			}
 		}
-		for(String test : starHashmap.keySet()) {
-			System.out.print(test);
-			for(int i : starHashmap.get(test)) {
-				System.out.print(i);
+		
+		for(String coordinates : starHashmap.keySet()) {			
+			if(starHashmap.get(coordinates).length == 2) {
+				ans += (starHashmap.get(coordinates)[0] * starHashmap.get(coordinates)[1]);
 			}
-			System.out.println();
+
 		}
 		
-		//starHashmap.forEach((x, y) -> System.out.println(x[0] + " " + y[0]));
 		System.out.println(ans);
 	}	
 	
 	public void putTempStarCoordinates(HashMap<String, Integer> tempStar, Integer i, Integer j) {
-		String temp = Integer.toString(i) + Integer.toString(j);
+		String temp = Integer.toString(i) + "." + Integer.toString(j);
 		if(!tempStar.containsKey(temp)) {
 			tempStar.put(temp, null);
 		}
